79c96c458d00f05238931bbf3ca6bf48171d8d0c
invitacionesauramx-ctrl <invitacionesauramx@gmail.com>
2026-02-24 17:37:28 -0600

    config.js

--- config.js
+++ config.js
@@ -1,45 +1,45 @@
 const CONFIG = {
 
-  nombre: "Marlen",
-  fechaTexto: "22 · 09 · 2026",
-  fechaCuentaRegresiva: "2026-12-15T17:00:00",
+  nombre: "Madre Alejandra",
+  fechaTexto: "25 · 02 · 2026",
+  fechaCuentaRegresiva: "2026-02-25T17:00:00",
   telefonoWhatsApp: "527531006781",
-  hashtag: "#XVLaura2026",
+  hashtag: "#XVMayra2026",
 
-  invitado: "Laura Salinas Garza",
+  invitado: "Daniela valdovinos",
 
   familia: {
-    nombre: "Familia Hernández López",
+    nombre: "Familia ortiz paniagua",
     integrantes: [
-      "Ana Hernández López",
-      "Miguel Hernández López",
-      "Sofía Hernández López",
-      "Daniel Hernández López"
+      "Anali ortiz paniagua",
+      "Flor ortiz paniagua",
+      "Pingo ortiz paniagua",
+      "Carlos ortiz paniagua"
     ]
   },
 
   padres: [
-    "Marca del Socorro",
-    "Pamela Castaño"
+    "Virginia gallo ramirez",
+    "chelis gallos ramirez"
   ],
 
   padrinos: [
-    "Maylet Perez Nuñez",
-    "Rosa Mendoza Salgado"
+    "Rosa Perez Nuñez",
+    "sofia Mendoza Salgado"
   ],
 
-  mensaje: "Hay momentos en la vida que son especiales por sí solos...",
+  mensaje: "Hay muchos enormes momentos en la vida que son especiales por sí solos...",
 
   ceremonia: {
-    lugar: "Iglesia la capilla del niño",
+    lugar: "Iglesia la esquina de mi casa",
     hora: "21:00 hrs",
-    direccion: "El Cerrito Valle #453",
-    ciudad: "Potasio, 60924",
+    direccion: "la bajada de mi casa #453",
+    ciudad: "Lázaro Cárdenas , 60924",
     mapa: "https://www.google.com/maps/search/Iglesia+Espiritu+Santo"
   },
 
   recepcion: {
-    lugar: "Zona Exclusiva",
+    lugar: "Zona zirahuen",
     hora: "20:00 hrs",
     direccion: "Av. Manuel L. Barragán 134",
     ciudad: "Anáhuac, 60957",
@@ -47,16 +47,16 @@ const CONFIG = {
   },
 
   itinerario: [
-    { hora: "17:00", titulo: "Ceremonia Religiosa", lugar: "Iglesia la capilla del niño" },
-    { hora: "19:00", titulo: "Presentación" },
-    { hora: "20:00", titulo: "Recepción", lugar: "Zona Exclusiva" },
-    { hora: "21:30", titulo: "Cena" },
-    { hora: "22:30", titulo: "Baile" }
+    { hora: "17:00", titulo: "Ceremonia vip", lugar: "Iglesia la capilla del niño" },
+    { hora: "19:00", titulo: "Presentación de novia" },
+    { hora: "20:00", titulo: "Recepción vip", lugar: "Zona Exclusiva" },
+    { hora: "21:30", titulo: "Cena vip" },
+    { hora: "22:30", titulo: "Baile vip" }
   ],
 
   regalos: {
-    beneficiario: "Liliana Sanchez",
-    banco: "BBVA",
+    beneficiario: "daniela ortiz",
+    banco: "sartander",
     cuenta: "12345678",
     clabe: "123456789",
     amazon: "https://www.amazon.com.mx",
