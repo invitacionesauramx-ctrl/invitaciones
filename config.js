f5816ec41806a50e8d422eeb2d699d26ec4be83a
invitacionesauramx-ctrl <invitacionesauramx@gmail.com>
2026-02-24 21:41:44 -0600

    config.js

--- config.js
+++ config.js
@@ -1,62 +1,62 @@
 const CONFIG = {
-  nombre: "Mayra Alejandra",
+  nombre: "Adriana bentacourt",
   fechaTexto: "24 · 02 · 2026",
-  fechaCuentaRegresiva: "2026-02-24T19:00:00",
+  fechaCuentaRegresiva: "2026-02-24T22:00:00",
   telefonoWhatsApp: "527531006781",
-  hashtag: "#XVMayra2026",
+  hashtag: "#XVAdriana2026",
 
-  invitado: "Daniela Valdovinos",
+  invitado: "Adrianna bentacourt",
 
   familia: {
-    nombre: "Familia Ortiz Paniagua",
+    nombre: "Familia bentacourt",
     telefono: "527531006781", // Número para WhatsApp en invitación
     integrantes: [
-      "Anali Ortiz Paniagua",
-      "Flor Ortiz Paniagua",
-      "Pingo Ortiz Paniagua",
-      "Carlos Ortiz Paniagua"
+      "Anali Ortiz bentacourt",
+      "Flor Ortiz bentacourt",
+      "Pingo Ortiz bentacourt",
+      "Carlos Ortiz bentacourt"
     ]
   },
 
   padres: [
-    "Virginia Gallo Ramirez",
-    "Chelis Gallos Ramirez"
+    "Virginia bentacourt",
+    "Chelis Gallos bentacourt"
   ],
 
   padrinos: [
-    "Rosa Perez Nuñez",
-    "Sofia Mendoza Salgado"
+    "Rosa Perez bentacourt",
+    "Sofia Mendoza bentacourt"
   ],
 
-  mensaje: "Hay muchos enormes momentos en la vida que son especiales por sí solos...",
+  mensaje: "Hay🥰🥰 muchos enormes momentos en la vida que son especiales por sí solos...",
 
   ceremonia: {
-    lugar: "Iglesia la esquina de mi casa",
+    lugar: "Iglesia la esquina de mi bentacourt",
     hora: "21:00 hrs",
-    direccion: "la bajada de mi casa #453",
+    direccion: "la bajada de mi bentacourt #453",
     ciudad: "Lázaro Cárdenas, 60924",
     mapa: "https://www.google.com/maps/search/Iglesia+Espiritu+Santo"
   },
 
   recepcion: {
-    lugar: "Zona Zirahuen",
+    lugar: "Zona bentacourt",
     hora: "20:00 hrs",
-    direccion: "Av. Manuel L. Barragán 134",
+    direccion: "Av. bentacourt L. Barragán 134",
     ciudad: "Anáhuac, 60957",
     mapa: "https://www.google.com/maps/search/Zona+Eventos+Anahuac+60957"
   },
 
   itinerario: [
-    { hora: "17:00", titulo: "Ceremonia VIP", lugar: "Iglesia la capilla del niño" },
-    { hora: "19:00", titulo: "Presentación de novia" },
-    { hora: "20:00", titulo: "Recepción VIP", lugar: "Zona Exclusiva" },
-    { hora: "21:30", titulo: "Cena VIP" },
-    { hora: "22:30", titulo: "Baile VIP" }
+    { hora: "17:00", titulo: "Ceremonia ben", lugar: "Iglesia la capilla del niño" },
+    { hora: "19:00", titulo: "Presentación de ben" },
+    { hora: "20:00", titulo: "Recepción ben", lugar: "Zona bentacourt" },
+    { hora: "21:30", titulo: "Cena ben" },
+    { hora: "22:30", titulo: "Baile ben" }
   ],
 
   regalos: {
-    beneficiario: "Daniela Ortiz",
-    banco: "Sartander",
+    beneficiario: "Daniela bentacourt",
+    banco: "banamex",
     cuenta: "12345678",
     clabe: "123456789",
     amazon: "https://www.amazon.com.mx",
@@ -65,12 +65,12 @@ const CONFIG = {
 
   sugerencias: {
     hospedaje: [
-      { nombre: "Hotel Pacifica", direccion: "Av. Noyola 123, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Paraiso" },
-      { nombre: "Hotel Colibri", direccion: "Calle Ferrocarriles 45, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Sol" }
+      { nombre: "Hotel bentacourt", direccion: "Av. Noyola 123, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Paraiso" },
+      { nombre: "Hotel bentacourt", direccion: "Calle Ferrocarriles 45, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Sol" }
     ],
     lugares: [
-      { nombre: "Parque Malecon", direccion: "Calle 1, Ciudad", mapa: "https://www.google.com/maps/search/Parque+Central" },
-      { nombre: "Plaza Zirahuen", direccion: "Avenida Cultura 22, Ciudad", mapa: "https://www.google.com/maps/search/Museo+de+Arte" }
+      { nombre: "Parque bentacourt", direccion: "Calle 1, Ciudad", mapa: "https://www.google.com/maps/search/Parque+Central" },
+      { nombre: "Plaza bentacourt", direccion: "Avenida Cultura 22, Ciudad", mapa: "https://www.google.com/maps/search/Museo+de+Arte" }
     ]
   }
 };
