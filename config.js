7e1f4186eca2b308670aa6b41902ff2afc270f61
invitacionesauramx-ctrl <invitacionesauramx@gmail.com>
2026-02-25 12:52:11 -0600

    config.js

--- config.js
+++ config.js
@@ -1,76 +1,76 @@
 const CONFIG = {
-  nombre: "Adriana bentacourt",
+  nombre: "Adriana Bentacourt",
   fechaTexto: "24 · 02 · 2026",
   fechaCuentaRegresiva: "2026-02-24T22:00:00",
   telefonoWhatsApp: "527531006781",
   hashtag: "#XVAdriana2026",
 
-  invitado: "Adrianna bentacourt",
+  invitado: "Adrianna Bentacourt",
 
   familia: {
-  nombre: "Familia bentacourt",
-  telefono: "527531006781",
-  miembros: [
-    "Anali Ortiz bentacourt",
-    "Flor Ortiz bentacourt",
-    "Pingo Ortiz bentacourt",
-    "Carlos Ortiz bentacourt"
-  ]
-},
+    nombre: "Familia Bentacourt",
+    telefono: "527531006781",
+    miembros: [
+      "Anali Ortiz Bentacourt",
+      "Flor Ortiz Bentacourt",
+      "Pingo Ortiz Bentacourt",
+      "Carlos Ortiz Bentacourt"
+    ]
+  },
 
   padres: [
-    "Virginia bentacourt",
-    "Chelis Gallos bentacourt"
+    "Virginia Bentacourt",
+    "Chelis Gallos Bentacourt"
   ],
 
   padrinos: [
-    "Rosa Perez bentacourt",
-    "Sofia Mendoza bentacourt"
+    "Rosa Pérez Bentacourt",
+    "Sofía Mendoza Bentacourt"
   ],
 
-  mensaje: "Hay❤ muchos enormes momentos en la vida que son especiales por sí solos...",
+  mensaje: "Hay momentos en la vida que son tan especiales que merecen ser compartidos con quienes más queremos…",
 
   ceremonia: {
-    lugar: "Iglesia la esquina de mi bentacourt",
-    hora: "21:00 hrs",
-    direccion: "la bajada de mi bentacourt #453",
+    lugar: "Iglesia La Esquina de Mi Bentacourt",
+    hora: "19:00 hrs",
+    direccion: "La Bajada de Mi Bentacourt #453",
     ciudad: "Lázaro Cárdenas, 60924",
     mapa: "https://www.google.com/maps/search/Iglesia+Espiritu+Santo"
   },
 
   recepcion: {
-    lugar: "Zona bentacourt",
+    lugar: "Zona Bentacourt",
     hora: "20:00 hrs",
-    direccion: "Av. bentacourt L. Barragán 134",
+    direccion: "Av. Bentacourt L. Barragán 134",
     ciudad: "Anáhuac, 60957",
     mapa: "https://www.google.com/maps/search/Zona+Eventos+Anahuac+60957"
   },
 
   itinerario: [
-    { hora: "17:00", titulo: "Ceremonia ben", lugar: "Iglesia la capilla del niño" },
-    { hora: "19:00", titulo: "Presentación de ben" },
-    { hora: "20:00", titulo: "Recepción ben", lugar: "Zona bentacourt" },
-    { hora: "21:30", titulo: "Cena ben" },
-    { hora: "22:30", titulo: "Baile ben" }
+    { hora: "17:00", titulo: "Ceremonia", lugar: "Iglesia la Capilla del Niño" },
+    { hora: "19:00", titulo: "Presentación" },
+    { hora: "20:00", titulo: "Recepción", lugar: "Zona Bentacourt" },
+    { hora: "21:30", titulo: "Cena" },
+    { hora: "22:30", titulo: "Baile" }
   ],
 
   regalos: {
-  beneficiario: "Daniela Bentacourt",
-  banco: "Banamex",
-  cuenta: "12345678",
-  clabe: "123456789",
-  amazon: "https://www.amazon.com.mx",
-  galeria: "https://drive.google.com/"
-},
+    beneficiario: "Daniela Bentacourt",
+    banco: "Banamex",
+    cuenta: "12345678",
+    clabe: "123456789",
+    amazon: "https://www.amazon.com.mx",
+    galeria: "https://drive.google.com/"
+  },
 
   sugerencias: {
-  hospedaje: [
-    { nombre: "Hotel Bentacourt", direccion: "Av. Noyola 123, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Bentacourt" },
-    { nombre: "Hotel Bentacourt 2", direccion: "Calle Ferrocarriles 45, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Bentacourt+2" }
-  ],
-  lugares: [
-    { nombre: "Parque Bentacourt", direccion: "Calle 1, Ciudad", mapa: "https://www.google.com/maps/search/Parque+Bentacourt" },
-    { nombre: "Plaza Bentacourt", direccion: "Avenida Cultura 22, Ciudad", mapa: "https://www.google.com/maps/search/Plaza+Bentacourt" }
-  ]
-}
+    hospedaje: [
+      { nombre: "Hotel Bentacourt", direccion: "Av. Noyola 123, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Bentacourt" },
+      { nombre: "Hotel Bentacourt 2", direccion: "Calle Ferrocarriles 45, Ciudad", mapa: "https://www.google.com/maps/search/Hotel+Bentacourt+2" }
+    ],
+    lugares: [
+      { nombre: "Parque Bentacourt", direccion: "Calle 1, Ciudad", mapa: "https://www.google.com/maps/search/Parque+Bentacourt" },
+      { nombre: "Plaza Bentacourt", direccion: "Avenida Cultura 22, Ciudad", mapa: "https://www.google.com/maps/search/Plaza+Bentacourt" }
+    ]
+  }
 };
